b3910db25187bf69a63267b7e251b89491bc70b6
lsamano <leizl.samano@gmail.com>
2019-01-16 14:00:11 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+    @dogs = Dog.all
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.assign_attributes(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Employee.find(params[:id]).destroy
+    redirect_to(employees_path)
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :dog_id)
+  end
 end
